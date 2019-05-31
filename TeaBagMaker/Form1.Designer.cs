ecce12bd60212bdbb50e83a9e401e935952db30a
Chaeyeon Bang <qkdcodus0117@gmail.com>
2019-05-31 15:07:38 +0900

    #1 timer, combo box, button, label 배치

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.teaComboBox = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.countDownBtn = new System.Windows.Forms.Button();
+            this.timeTxt = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -43,48 +43,57 @@
             this.teaComboBox.Name = "teaComboBox";
             this.teaComboBox.Size = new System.Drawing.Size(225, 22);
             this.teaComboBox.TabIndex = 0;
+            this.teaComboBox.SelectedIndexChanged += new System.EventHandler(this.TeaComboBox_SelectedIndexChanged);
             // 
-            // button1
+            // countDownBtn
             // 
-            this.button1.Location = new System.Drawing.Point(12, 44);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(223, 92);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.countDownBtn.Location = new System.Drawing.Point(12, 44);
+            this.countDownBtn.Name = "countDownBtn";
+            this.countDownBtn.Size = new System.Drawing.Size(223, 92);
+            this.countDownBtn.TabIndex = 1;
+            this.countDownBtn.Text = "카운트";
+            this.countDownBtn.UseVisualStyleBackColor = true;
+            this.countDownBtn.Click += new System.EventHandler(this.CountDownBtn_Click);
             // 
-            // label1
+            // timeTxt
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 140);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 14);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.timeTxt.AutoSize = true;
+            this.timeTxt.Font = new System.Drawing.Font("나눔고딕OTF", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.timeTxt.Location = new System.Drawing.Point(12, 140);
+            this.timeTxt.Name = "timeTxt";
+            this.timeTxt.Size = new System.Drawing.Size(108, 24);
+            this.timeTxt.TabIndex = 2;
+            this.timeTxt.Text = "남은 시간: ";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(201, 261);
+            this.label2.Location = new System.Drawing.Point(101, 213);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 14);
+            this.label2.Size = new System.Drawing.Size(138, 14);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Text = "2019-05-31 3108 방채연";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(247, 286);
+            this.ClientSize = new System.Drawing.Size(247, 236);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.timeTxt);
+            this.Controls.Add(this.countDownBtn);
             this.Controls.Add(this.teaComboBox);
             this.Font = new System.Drawing.Font("나눔고딕OTF", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,8 +102,8 @@
         #endregion
 
         private System.Windows.Forms.ComboBox teaComboBox;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button countDownBtn;
+        private System.Windows.Forms.Label timeTxt;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label2;
     }
--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -12,9 +12,71 @@ namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
+        String[] teaList = { "홍차", "녹차", "루이보스차", "국화차" };
+        String select = "";
+        int time = 0;
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < teaList.Length; i++)
+            {
+                this.teaComboBox.Items.Add(teaList[i]);
+            }
+
+            if(select.Equals(""))
+            {
+                this.teaComboBox.SelectedIndex = 0;
+            }
+        }
+
+        private void CountDownBtn_Click(object sender, EventArgs e)
+        {
+            this.timer1.Enabled = true;
+        }
+
+        private void TeaComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            select = teaComboBox.Text;
+            switch (select)
+            {
+                case "홍차":
+                    time = 120;
+                    break;
+                case "녹차":
+                    time = 180;
+                    break;
+                case "루이보스차":
+                    time = 300;
+                    break;
+                case "국화차":
+                    time = 120;
+                    break;
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            int min, sec;
+
+            if (time == 0)
+            {
+                this.timer1.Enabled = false;
+                MessageBox.Show("티백을 건지세요!", "종료",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.teaComboBox.Focus();
+            }
+            else
+            {
+                time--;
+                min = time / 60;
+                sec = time % 60;
+                timeTxt.Text = "남은 시간: " + min + ":" + sec;
+            }
         }
     }
 }
